66a011f9a49e8abb9467da4fa9ea59966cda5f37
yuanfeng1 <yuanfeng1@fun.tv>
2018-07-23 15:56:10 +0800

    修改 Android_grayBitmap 实现。

--- android/android/jni/imgalgth/imgalgth.cpp
+++ android/android/jni/imgalgth/imgalgth.cpp
@@ -14,7 +14,7 @@ __BEGIN_DECLS
 STDCEXPORT void Android_grayBitmap(void* pixels, uint32_t width, uint32_t height)
 {
     assert(pixels);
-    __NS::handleBitmap((__NS::Color*)pixels, width, height, [](__NS::Color& color) { color.red = color.green = color.blue = (uint8_t)(0.30f * color.red + 0.59f * color.green + 0.11f * color.blue); });
+    __NS::handleBitmap((__NS::Color*)pixels, width, height, [](__NS::Color& color) { color.red = color.green = color.blue = (uint8_t)(0.299f * color.red + 0.587f * color.green + 0.114f * color.blue); });
 }
 
 ///////////////////////////////////////////////////////////////////////////////
