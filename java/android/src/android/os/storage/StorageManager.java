03cd0c338787161f9abab6699bf9740a22d3eacc
yuanfeng1 <yuanfeng1@fun.tv>
2019-04-19 16:27:21 +0800

    修改 android internal 包

--- java/android/src/android/os/storage/StorageManager.java
+++ java/android/src/android/os/storage/StorageManager.java
@@ -1,19 +1,3 @@
-/*
- * Copyright (C) 2008 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package android.os.storage;
 
 import java.io.File;
@@ -22,256 +6,92 @@ import android.content.Context;
 import android.os.Looper;
 import android.os.RemoteException;
 
-/**
- * StorageManager is the interface to the systems storage service. The storage
- * manager handles storage-related items such as Opaque Binary Blobs (OBBs).
- * <p>
- * OBBs contain a filesystem that maybe be encrypted on disk and mounted
- * on-demand from an application. OBBs are a good way of providing large amounts
- * of binary assets without packaging them into APKs as they may be multiple
- * gigabytes in size. However, due to their size, they're most likely stored in
- * a shared storage pool accessible from all programs. The system does not
- * guarantee the security of the OBB file itself: if any program modifies the
- * OBB, there is no guarantee that a read from that OBB will produce the
- * expected output.
- * <p>
- * Get an instance of this class by calling
- * {@link android.content.Context#getSystemService(java.lang.String)} with an
- * argument of {@link android.content.Context#STORAGE_SERVICE}.
- */
 public class StorageManager {
-    /** {@hide} */
     public static StorageManager from(Context context) {
         throw new UnsupportedOperationException();
     }
 
-    /**
-     * Constructs a StorageManager object through which an application can
-     * can communicate with the systems mount service.
-     * 
-     * @param tgtLooper The {@link android.os.Looper} which events will be received on.
-     *
-     * <p>Applications can get instance of this class by calling
-     * {@link android.content.Context#getSystemService(java.lang.String)} with an argument
-     * of {@link android.content.Context#STORAGE_SERVICE}.
-     *
-     * @hide
-     */
     public StorageManager(ContentResolver resolver, Looper tgtLooper) throws RemoteException {
         throw new UnsupportedOperationException();
     }
 
-    /**
-     * Registers a {@link android.os.storage.StorageEventListener StorageEventListener}.
-     *
-     * @param listener A {@link android.os.storage.StorageEventListener StorageEventListener} object.
-     *
-     * @hide
-     */
     public void registerListener(StorageEventListener listener) {
         throw new UnsupportedOperationException();
     }
 
-    /**
-     * Unregisters a {@link android.os.storage.StorageEventListener StorageEventListener}.
-     *
-     * @param listener A {@link android.os.storage.StorageEventListener StorageEventListener} object.
-     *
-     * @hide
-     */
     public void unregisterListener(StorageEventListener listener) {
         throw new UnsupportedOperationException();
     }
 
-    /**
-     * Enables USB Mass Storage (UMS) on the device.
-     *
-     * @hide
-     */
     public void enableUsbMassStorage() {
         throw new UnsupportedOperationException();
     }
 
-    /**
-     * Disables USB Mass Storage (UMS) on the device.
-     *
-     * @hide
-     */
     public void disableUsbMassStorage() {
         throw new UnsupportedOperationException();
     }
 
-    /**
-     * Query if a USB Mass Storage (UMS) host is connected.
-     * @return true if UMS host is connected.
-     *
-     * @hide
-     */
     public boolean isUsbMassStorageConnected() {
         throw new UnsupportedOperationException();
     }
 
-    /**
-     * Query if a USB Mass Storage (UMS) is enabled on the device.
-     * @return true if UMS host is enabled.
-     *
-     * @hide
-     */
     public boolean isUsbMassStorageEnabled() {
         throw new UnsupportedOperationException();
     }
 
-    /**
-     * Mount an Opaque Binary Blob (OBB) file. If a <code>key</code> is
-     * specified, it is supplied to the mounting process to be used in any
-     * encryption used in the OBB.
-     * <p>
-     * The OBB will remain mounted for as long as the StorageManager reference
-     * is held by the application. As soon as this reference is lost, the OBBs
-     * in use will be unmounted. The {@link OnObbStateChangeListener} registered
-     * with this call will receive the success or failure of this operation.
-     * <p>
-     * <em>Note:</em> you can only mount OBB files for which the OBB tag on the
-     * file matches a package ID that is owned by the calling program's UID.
-     * That is, shared UID applications can attempt to mount any other
-     * application's OBB that shares its UID.
-     * 
-     * @param rawPath the path to the OBB file
-     * @param key secret used to encrypt the OBB; may be <code>null</code> if no
-     *            encryption was used on the OBB.
-     * @param listener will receive the success or failure of the operation
-     * @return whether the mount call was successfully queued or not
-     */
     public boolean mountObb(String rawPath, String key, OnObbStateChangeListener listener) {
         throw new UnsupportedOperationException();
     }
 
-    /**
-     * Unmount an Opaque Binary Blob (OBB) file asynchronously. If the
-     * <code>force</code> flag is true, it will kill any application needed to
-     * unmount the given OBB (even the calling application).
-     * <p>
-     * The {@link OnObbStateChangeListener} registered with this call will
-     * receive the success or failure of this operation.
-     * <p>
-     * <em>Note:</em> you can only mount OBB files for which the OBB tag on the
-     * file matches a package ID that is owned by the calling program's UID.
-     * That is, shared UID applications can obtain access to any other
-     * application's OBB that shares its UID.
-     * <p>
-     * 
-     * @param rawPath path to the OBB file
-     * @param force whether to kill any programs using this in order to unmount
-     *            it
-     * @param listener will receive the success or failure of the operation
-     * @return whether the unmount call was successfully queued or not
-     */
     public boolean unmountObb(String rawPath, boolean force, OnObbStateChangeListener listener) {
         throw new UnsupportedOperationException();
     }
 
-    /**
-     * Check whether an Opaque Binary Blob (OBB) is mounted or not.
-     * 
-     * @param rawPath path to OBB image
-     * @return true if OBB is mounted; false if not mounted or on error
-     */
     public boolean isObbMounted(String rawPath) {
         throw new UnsupportedOperationException();
     }
 
-    /**
-     * Check the mounted path of an Opaque Binary Blob (OBB) file. This will
-     * give you the path to where you can obtain access to the internals of the
-     * OBB.
-     * 
-     * @param rawPath path to OBB image
-     * @return absolute path to mounted OBB image data or <code>null</code> if
-     *         not mounted or exception encountered trying to read status
-     */
     public String getMountedObbPath(String rawPath) {
         throw new UnsupportedOperationException();
     }
 
-    /**
-     * Gets the state of a volume via its mountpoint.
-     * @hide
-     */
     public String getVolumeState(String mountPoint) {
         throw new UnsupportedOperationException();
     }
 
-    /**
-     * Returns list of all mountable volumes.
-     * @hide
-     */
     public StorageVolume[] getVolumeList() {
         throw new UnsupportedOperationException();
     }
 
-    /**
-     * Returns list of paths for all mountable volumes.
-     * @hide
-     */
     public String[] getVolumePaths() {
         throw new UnsupportedOperationException();
     }
 
-    /** {@hide} */
     public StorageVolume getPrimaryVolume() {
         throw new UnsupportedOperationException();
     }
 
-    /** {@hide} */
     public static StorageVolume getPrimaryVolume(StorageVolume[] volumes) {
         throw new UnsupportedOperationException();
     }
 
-    /**
-     * Return the number of available bytes until the given path is considered
-     * running low on storage.
-     *
-     * @hide
-     */
     public long getStorageBytesUntilLow(File path) {
         throw new UnsupportedOperationException();
     }
 
-    /**
-     * Return the number of available bytes at which the given path is
-     * considered running low on storage.
-     *
-     * @hide
-     */
     public long getStorageLowBytes(File path) {
         throw new UnsupportedOperationException();
     }
 
-    /**
-     * Return the number of available bytes at which the given path is
-     * considered full.
-     *
-     * @hide
-     */
     public long getStorageFullBytes(File path) {
         throw new UnsupportedOperationException();
     }
 
-    /// Consts to match the password types in cryptfs.h
-    /** @hide */
     public static final int CRYPT_TYPE_PASSWORD = 0;
-    /** @hide */
     public static final int CRYPT_TYPE_DEFAULT = 1;
-    /** @hide */
     public static final int CRYPT_TYPE_PATTERN = 2;
-    /** @hide */
     public static final int CRYPT_TYPE_PIN = 3;
-
-    // Constants for the data available via MountService.getField.
-    /** @hide */
     public static final String SYSTEM_LOCALE_KEY = "SystemLocale";
-    /** @hide */
     public static final String OWNER_INFO_KEY = "OwnerInfo";
-    /** @hide */
     public static final String PATTERN_VISIBLE_KEY = "PatternVisible";
 }
--- java/android/src/android/os/storage/StorageVolume.java
+++ java/android/src/android/os/storage/StorageVolume.java
@@ -1,19 +1,3 @@
-/*
- * Copyright (C) 2011 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package android.os.storage;
 
 import java.io.File;
@@ -23,22 +7,11 @@ import android.os.Parcel;
 import android.os.Parcelable;
 import android.os.UserHandle;
 
-/**
- * Description of a storage volume and its capabilities, including the
- * filesystem path where it may be mounted.
- *
- * @hide
- */
 @SuppressLint("NewApi")
 public class StorageVolume implements Parcelable {
-    // StorageVolume extra for ACTION_MEDIA_REMOVED, ACTION_MEDIA_UNMOUNTED, ACTION_MEDIA_CHECKING,
-    // ACTION_MEDIA_NOFS, ACTION_MEDIA_MOUNTED, ACTION_MEDIA_SHARED, ACTION_MEDIA_UNSHARED,
-    // ACTION_MEDIA_BAD_REMOVAL, ACTION_MEDIA_UNMOUNTABLE and ACTION_MEDIA_EJECT broadcasts.
     public static final String EXTRA_STORAGE_VOLUME = "storage_volume";
 
-    public StorageVolume(File path, int descriptionId, boolean primary, boolean removable,
-            boolean emulated, int mtpReserveSpace, boolean allowMassStorage, long maxFileSize,
-            UserHandle owner) {
+    public StorageVolume(File path, int descriptionId, boolean primary, boolean removable, boolean emulated, int mtpReserveSpace, boolean allowMassStorage, long maxFileSize, UserHandle owner) {
         throw new UnsupportedOperationException();
     }
 
@@ -46,11 +19,6 @@ public class StorageVolume implements Parcelable {
         throw new UnsupportedOperationException();
     }
 
-    /**
-     * Returns the mount path for the volume.
-     *
-     * @return the mount path
-     */
     public String getPath() {
         throw new UnsupportedOperationException();
     }
@@ -59,11 +27,6 @@ public class StorageVolume implements Parcelable {
         throw new UnsupportedOperationException();
     }
 
-    /**
-     * Returns a user visible description of the volume.
-     *
-     * @return the volume description
-     */
     public String getDescription(Context context) {
         throw new UnsupportedOperationException();
     }
@@ -76,71 +39,30 @@ public class StorageVolume implements Parcelable {
         throw new UnsupportedOperationException();
     }
 
-    /**
-     * Returns true if the volume is removable.
-     *
-     * @return is removable
-     */
     public boolean isRemovable() {
         throw new UnsupportedOperationException();
     }
 
-    /**
-     * Returns true if the volume is emulated.
-     *
-     * @return is removable
-     */
     public boolean isEmulated() {
         throw new UnsupportedOperationException();
     }
 
-    /**
-     * Returns the MTP storage ID for the volume.
-     * this is also used for the storage_id column in the media provider.
-     *
-     * @return MTP storage ID
-     */
     public int getStorageId() {
         throw new UnsupportedOperationException();
     }
 
-    /**
-     * Do not call this unless you are MountService
-     */
     public void setStorageId(int index) {
         throw new UnsupportedOperationException();
     }
 
-    /**
-     * Number of megabytes of space to leave unallocated by MTP.
-     * MTP will subtract this value from the free space it reports back
-     * to the host via GetStorageInfo, and will not allow new files to
-     * be added via MTP if there is less than this amount left free in the storage.
-     * If MTP has dedicated storage this value should be zero, but if MTP is
-     * sharing storage with the rest of the system, set this to a positive value
-     * to ensure that MTP activity does not result in the storage being
-     * too close to full.
-     *
-     * @return MTP reserve space
-     */
     public int getMtpReserveSpace() {
         throw new UnsupportedOperationException();
     }
 
-    /**
-     * Returns true if this volume can be shared via USB mass storage.
-     *
-     * @return whether mass storage is allowed
-     */
     public boolean allowMassStorage() {
         throw new UnsupportedOperationException();
     }
 
-    /**
-     * Returns maximum file size for the volume, or zero if it is unbounded.
-     *
-     * @return maximum file size
-     */
     public long getMaxFileSize() {
         throw new UnsupportedOperationException();
     }
@@ -157,10 +79,6 @@ public class StorageVolume implements Parcelable {
         throw new UnsupportedOperationException();
     }
 
-    /**
-     * Parse and return volume UUID as FAT volume ID, or return -1 if unable to
-     * parse or UUID is unknown.
-     */
     public int getFatVolumeId() {
         throw new UnsupportedOperationException();
     }
@@ -181,39 +99,32 @@ public class StorageVolume implements Parcelable {
         throw new UnsupportedOperationException();
     }
 
-    @Override
     public boolean equals(Object obj) {
         throw new UnsupportedOperationException();
     }
 
-    @Override
     public int hashCode() {
         throw new UnsupportedOperationException();
     }
 
-    @Override
     public String toString() {
         throw new UnsupportedOperationException();
     }
 
     public static final Creator<StorageVolume> CREATOR = new Creator<StorageVolume>() {
-        @Override
         public StorageVolume createFromParcel(Parcel in) {
             throw new UnsupportedOperationException();
         }
 
-        @Override
         public StorageVolume[] newArray(int size) {
             throw new UnsupportedOperationException();
         }
     };
 
-    @Override
     public int describeContents() {
-        return 0;
+        throw new UnsupportedOperationException();
     }
 
-    @Override
     public void writeToParcel(Parcel parcel, int flags) {
         throw new UnsupportedOperationException();
     }
