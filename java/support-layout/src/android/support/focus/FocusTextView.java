652f495f12ca127ddb291cb02f3548d5feedb445
yuanfeng1 <yuanfeng1@fun.tv>
2019-06-20 17:21:43 +0800

    增加 FocusTextView 类

---
+++ java/support-layout/src/android/support/focus/FocusTextView.java
@@ -0,0 +1,29 @@
+package android.support.focus;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+/**
+ * Class <tt>FocusTextView</tt>
+ * @author Garfield
+ */
+public class FocusTextView extends TextView {
+    private final FocusDrawable mDrawable;
+
+    public FocusTextView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public FocusTextView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        mDrawable = new FocusDrawable(context, attrs);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        mDrawable.draw(canvas, this, ENABLED_FOCUSED_STATE_SET);
+    }
+}
