72e314c465fdaaef68ccccf762acbe24fbc243e0
yuanfeng1 <yuanfeng1@fun.tv>
2020-09-02 14:19:42 +0800

    更新 zxing 库

--- android/android/jni/Application.mk
+++ android/android/jni/Application.mk
@@ -1,7 +1,11 @@
 # The ARMv7 is significanly faster due to the use of the hardware FPU
-# APP_ABI := arm64-v8a mips64 x86_64
-# APP_ABI := armeabi armeabi-v7a x86 mips
+# APP_ABI := arm64-v8a armeabi-v7a x86_64 x86
 
-APP_ABI := armeabi-v7a arm64-v8a
+ifeq ($(origin NDK_ABI), undefined)
+APP_ABI := all
+else
+APP_ABI := $(NDK_ABI)
+endif
+
 APP_PLATFORM := android-16
 APP_CPPFLAGS += -fno-rtti -fno-exceptions -std=c++11
